e8c18305d4d52a81bbebef75675d0d3a48bc7f7a
Fungus Humungus <fungus.humungus@gmail.com>
2015-06-18 21:41:20 +0100

    Added more tests.
    We are now testing Chains and  Apply. Not all the Chain tests could be
    implemented as some were using methods we aren't wrapping (so not
    necessary to test).

--- spec/antonio/element_spec.clj
+++ spec/antonio/element_spec.clj
@@ -1,5 +1,6 @@
 (ns antonio.element-spec
   (:require [speclj.core :refer :all]
+            [antonio.macros :refer :all]
             [antonio.language :refer :all]
             [antonio.library.atomic.continuous :refer :all]
             [antonio.equals :refer :all]))
@@ -67,10 +68,10 @@
                 (.set f2 true)
                 (.set f3 false)
                 (.generate c)
-                (should (= (.value c) false))
+                (should (= false (.value c)))
                 (.set f1 true)
                 (.generate c)
-                (should= (.value c) true)))
+                (should= true (.value c))))
 
           (it "should return a cached result in"
               (with-local-vars [sum 0]
@@ -81,7 +82,7 @@
                   (.get c true)
                   (.get c false)
                   (.get c true)
-                  (should= @sum 2))))
+                  (should= 2 @sum))))
 
           #_( ;; Where is this Uniform with 3 parameters?
             it "should call the CPD when the cache is full"
@@ -92,16 +93,6 @@
                           (Constant b))
                       f1 (Uniform 0 1 2)
                       c (NonCachingChain f1 f)]))))
-
-(describe "A chain managing the context"
-          #_( ;; Private vars..
-             it "should store new elements in the correct subContext"
-              (.createNew Universe)
-              (let [c (Chain (Flip 0.5)
-                             (fn [b] (if b (Constant 0) (Constant 1))))]
-                (.get c true)
-                (.get c false)
-                (should= (-> c (.myMappedContextContents true) .size) 1))))
 
 (describe "A chain with two parents non-caching"
           (it "should have value equal to the value of its function applied to its argument's values"
@@ -116,6 +107,112 @@
                 (.set f3 false)
                 (.set f4 true)
                 (.generate c)
-                (should= (.value c) true))))
+                (should= true (.value c))
+                (.set f1 true)
+                (.generate c)
+                (should= false (.value c)))))
+
+(describe "A chain with two parents caching"
+          (it "should have value equal to the value of its function applied to its argument's value"
+              (.createNew Universe)
+              (let [f1 (Flip 0.7)
+                    f2 (Flip 0.4)
+                    f3 (Flip 0.9)
+                    f4 (Flip 0.235)]
+                (.set f1 false)
+                (.set f2 true)
+                (.set f3 false)
+                (.set f4 true)
+                (let [c (CachingChain f1 f2 (fn [b1 b2]
+                                              (if (and b1 b2) f3 f4)))]
+                  (.generate c)
+                  (should= (.value c) true)
+                  (.set f1 true)
+                  (.generate c)
+                  (should= false (.value c)))))
+
+          (it "should evaluate the CPD only once for each input"
+              (with-local-vars [sum 0]
+                (letfn [(f [b1 b2]
+                          (var-set sum (inc @sum))
+                          (Constant (and b1 b2)))]
+                  (let [f1 (Flip 0.5)
+                        f2 (Flip 0.5)]
+                    (.set f1 true)
+                    (.set f2 true)
+                    (let [c (CachingChain f1 f2 f)]
+                      (.get c (tuple true true))
+                      (.get c (tuple false false))
+                      (.get c (tuple true true))
+                      (should= 2 @sum))))) ))
+
+(describe "An Apply with one argument"
+          (it "should have value equal to its function applied to its argument"
+              (.createNew Universe)
+              (let [u (Uniform 0 2)
+                    a (Apply u (fn [d] (inc d)))]
+                (.set u 1.3)
+                (.generate a)
+                (should= 2.3 (.value a)))))
+
+(describe "An Apply with two arguments"
+          (it "should have value equal to its function applied to its arguments"
+              (.createNew Universe)
+              (let [u (Uniform 0 2)
+                    v (Constant 1)
+                    a (Apply u v (fn [d1 d2] (+ d1 d2 1)))]
+                (.set u 1.3)
+                (.set v 1)
+                (.generate a)
+                (should= 3.3 (.value a)))))
+
+(describe "An Apply with three arguments"
+          (it "should have value equal to its function applied to its arguments"
+              (.createNew Universe)
+              (let [u (Uniform 0 2)
+                    v (Constant 1)
+                    w (Select [[0.5 0]
+                               [0.5 5]])
+                    a (Apply u v w (fn [d1 d2 d3] (+ d1 d2 d3 1)))]
+                (.set u 1.3)
+                (.set v 1)
+                (.set w 5)
+                (.generate a)
+                (should= 8.3 (.value a)))))
+
+(describe "An Apply with four arguments"
+          (it "should have value equal to its function applied to its arguments"
+              (.createNew Universe)
+              (let [u (Uniform 0 2)
+                    v (Constant 1)
+                    w (Select [[0.5 0]
+                               [0.5 5]])
+                    x (Constant -2)
+                    a (Apply u v w x (fn [d1 d2 d3 d4] (+ d1 d2 d3 d4 1)))]
+                (.set u 1.3)
+                (.set v 1)
+                (.set w 5)
+                (.set x -2)
+                (.generate a)
+                (should= 6.3 (.value a)))))
+
+(describe "An Apply with five arguments"
+          (it "should have value equal to its function applied to its arguments"
+              (.createNew Universe)
+              (let [u (Uniform 0 2)
+                    v (Constant 1)
+                    w (Select [[0.5 0]
+                               [0.5 5]])
+                    x (Constant -2)
+                    y (Constant 0.5)
+                    a (Apply u v w x y (fn [d1 d2 d3 d4 d5] (+ d1 d2 d3 d4 d5 1)))]
+                (.set u 1.3)
+                (.set v 1)
+                (.set w 5)
+                (.set x -2)
+                (.set y 0.5)
+                (.generate a)
+                (should= 6.8 (.value a)))))
+
 
 (run-specs)
--- src/antonio/macros.clj
+++ src/antonio/macros.clj
@@ -1,7 +1,13 @@
 (ns antonio.macros
   (:require [clojure.walk :refer [prewalk-replace]]
+            [t6.from-scala.core :refer [$ $$] :as $]
+            [t6.from-scala.utils :as scala]
             [antonio.defaults :refer [*name* *universe*]]))
 
+(defn tuple
+  "Make it nicer to create scala tuples"
+  [& vals]
+  (apply $/tuple vals))
 
 (defn gen-params
   "Given a list of parameters in the form '(% (dosomething  %)) returns a list of vectors of generated symbols to forms with the %n replaced by the symbol"
